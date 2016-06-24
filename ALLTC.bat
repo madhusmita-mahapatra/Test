de58ad884c4361defe8e16a65fda2881e15778d5
madhusmita-mahapatra <madhusmitamahapatra.2007@gmail.com>
2016-06-24 13:38:52 +0530

    Update ALLTC.bat

--- ALLTC.bat
+++ ALLTC.bat
@@ -1,4 +1,4 @@
 d:
-cd jsonfiles
+cd gitrepo\Test
 newman -c InnSupport_ImplementationAPI.json -e innSupport_implementationAPI_Environment.json -E innSupport_implementationAPI_Environment.json -H Output
 
