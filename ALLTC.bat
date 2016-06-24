7d64b559417ed13b136c0ae39f5ad0099a954131
madhusmita-mahapatra <madhusmitamahapatra.2007@gmail.com>
2016-06-24 16:03:20 +0530

    Delete ALLTC.bat

--- ALLTC.bat
+++
@@ -1,5 +0,0 @@
-d:
-cd gitrepo
-cd Test
-newman -c postman_test_implementationApi.json -e postman_environmentVariable_implementationApi.json -E postman_environmentVariable_implementationApi.json -H Output
-
